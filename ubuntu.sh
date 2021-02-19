dcd3baac17398f23011a2361931750f824a80976
Patrick Meenan <PatMeenan@gmail.com>
2021-02-19 10:22:24 -0500

    Removed python from the install

--- ubuntu.sh
+++ ubuntu.sh
@@ -24,17 +24,7 @@ done
 until sudo apt-get install -y git screen nginx beanstalkd zip unzip curl \
     php-fpm php-apcu php-sqlite3 php-curl php-gd php-zip php-mbstring php-xml \
     imagemagick ffmpeg libjpeg-turbo-progs libimage-exiftool-perl \
-    software-properties-common python2.7 python-pip python-numpy python-scipy \
-    psmisc
-do
-    sleep 1
-done
-sudo apt-get install -y python-software-properties
-until sudo pip install --upgrade pip
-do
-    sleep 1
-done
-until sudo pip install monotonic pillow psutil requests ujson pyssim
+    software-properties-common psmisc
 do
     sleep 1
 done
